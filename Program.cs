751df974fdcda7f8d41c1dd006626b26916ecdb8
sealanguage <elaine.froehlich@gmail.com>
2019-05-11 10:52:51 -0400

    Initial check-in of module ThisDoesntWork

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FinalVCGithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello github");
+            Console.WriteLine("I hate you");
+            Console.WriteLine("xxxxxxxxxxxu");
+            Console.WriteLine("I hate you");
+        }
+    }
+}
+
